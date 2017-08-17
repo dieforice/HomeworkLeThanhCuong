eecab7367e73cc1f32ee6675079f705065f00aa4
DESKTOP-KBSLCQ1\Windows 10 TIMT <dieforice@gmail.com>
2017-08-17 20:08:34 +0700

    Session 8 The Number AI Homework

---
+++ Session8/The Number AI/Assets/Scripts/GameController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text displayText;
+	public Button chooseLower;
+	public Button chooseYes;
+	public Button chooseHigher;
+	public Button playAgain;
+	int guessNumber;
+	int max = 100;
+	int min = 0;
+	int guessCount=0;
+	string guess;
+
+
+
+	void Start () {
+		GuessNumber ();
+	}
+	public void GuessNumber()
+	{
+		guessNumber = Random.Range (min, max);
+		displayText.text = "Pick a number between 0 and 100, I guess the number is " + guessNumber;
+		guessCount++;
+		playAgain.gameObject.SetActive (false);
+	}
+	public void ChooseLower()
+	{
+		max = guessNumber-1;
+		guessNumber = Random.Range (min, max);
+		displayText.text = "A lower number? Is it " + guessNumber + " ?";
+		guessCount++;
+	}
+	public void ChooseHigher()
+	{
+		min = guessNumber+1;
+		guessNumber = Random.Range (min, max);
+		guessCount++;
+		if (guessNumber + 1 > 100) 
+		{
+			guessNumber = 100;
+		}
+		displayText.text = "A higher number? Is it " + guessNumber + " ?";
+	}
+	public void ChooseYes()
+	{
+		playAgain.gameObject.SetActive (true);
+		if (guessCount > 1) 
+		{
+			displayText.text = "I got the right one after " + guessCount + " times";
+		} 
+		else 
+		{
+			displayText.text = "Flawless! I only need " + guessCount + " to predict your number";
+		}
+	}
+	public void PlayAgain()
+	{
+		max = 100;
+		min = 0;
+		guessCount = 0;
+		GuessNumber ();
+	}
+}
